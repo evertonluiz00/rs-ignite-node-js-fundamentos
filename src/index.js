23f65b28785a6eb93e3165438a634ac1b74e0a0b
Everton Christmann <evertonluiz00@hotmail.com>
2021-11-01 12:50:24 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,34 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Olá Node - Json 22222" });
+app.use(express.json());
+
+// GET
+app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query);
+    return response.json(["Curso1", "Curso2", "Curso3"]);
+});
+
+// POST
+app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
+});
+
+// PUT
+app.put("/courses/:id", (request, response) => {
+    const params = request.params;
+    const { id } = request.params;
+    console.log(params);
+    console.log(id);
+    return response.json(["Curso6", "Curso2", "Curso3", "Curso4"]);
+});
+
+// DELETE
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso6", "Curso2", "Curso4"]);
 });
 
 app.listen(3333);
