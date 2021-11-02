4dbd78f348b6d988f9e952a5ea8b6c7b0ac88171
Everton Christmann <evertonluiz00@hotmail.com>
2021-11-01 23:58:50 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
-const { request, response } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+const customers = [];
+
 app.use(express.json());
 
+
 // GET
-app.get("/courses", (request, response) => {
-    const query = request.query;
-    console.log(query);
-    return response.json(["Curso1", "Curso2", "Curso3"]);
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statement);
 });
 
+
 // POST
-app.post("/courses", (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const existsCustomerByCpf = customers.some((customer) => customer.cpf === cpf);
+
+    if (existsCustomerByCpf) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id : uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
 });
+
 
 // PUT
 app.put("/courses/:id", (request, response) => {
